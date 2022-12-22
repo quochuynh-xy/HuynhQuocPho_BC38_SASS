dbb5a955156383c666dc16685610541d8ebb1b0f
huynhquocpho <huynhquocpho@gmail.com>
2022-12-22 09:28:57 +0700

    'COMMIT'

--- js/components.js
+++ js/components.js
@@ -3,24 +3,10 @@ window.onscroll = () => {
 }
 let headerChange = () => {
     let header = document.querySelector('header.header');
-    let headerLogo = document.querySelector('.header .container .site-logo img');
-    let headerContainer = document.querySelector('header .container.d-flex');
-    let headerContactBtn = document.querySelector('.header .container .nav-bar .nav-contact .contact-btn');
-    let menuDot = document.querySelector('.header .container .nav-bar .nav-menu ul .nav-col .menu-item::after');
     if(document.body.scrollTop > 20 || document.documentElement.scrollTop > 20 ) {
-        header.style.backgroundColor = "#fff"
-        headerLogo.style.display = "none";
-        headerContainer.style.padding = "6px 0";
-        headerContactBtn.style.height = "2.5rem";
-        headerContactBtn.style.lineHeight = "2.5rem";
-        // menuDot.style.transform = "translateY(-50%)";
+        header.classList.add("scrolling")
     } else {
-        header.style.backgroundColor = "unset"
-        headerLogo.style.display = "unset";
-        headerContainer.style.padding = "unset";
-        headerContactBtn.style.height = "3.5rem";
-        headerContactBtn.style.lineHeight = "3.5rem";
-        // menuDot.style.bottom = "initial";
+       header.classList.remove("scrolling");
     }
 }
 function resizeMenu() {
